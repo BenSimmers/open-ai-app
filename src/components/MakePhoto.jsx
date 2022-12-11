827efe960333bc104f120b785a42fd28533475f5
Benjamin Simmers <bej1380@gmail.com>
2022-12-11 16:48:02 +1000

    added changes

--- src/components/MakePhoto.jsx
+++ src/components/MakePhoto.jsx
@@ -7,7 +7,7 @@ const MakePhoto = (props) => {
   const [prompt, setPrompt] = useState('');
   const [n, setN] = useState(1);
 
-  const apiKey = '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';
+  const apiKey = '';
 
   const generateImages = () => {
     const url = 'https://api.openai.com/v1/images/generations';
@@ -17,8 +17,8 @@ const MakePhoto = (props) => {
     };
     const data = {
       prompt: prompt,
-      n: n,
-      size: '1024x1024',
+      n: 1,
+      size: '512x512',
     };
 
     fetch(url, {
@@ -33,35 +33,48 @@ const MakePhoto = (props) => {
   return (
     <div>
 
-      <h1>Make a Photo</h1>
-      <p>Click the button to generate a photo</p>
+        <h1 className="text-4xl lg:max-w-3xl tracking-tight font-bold text-white sm:text-5xl sm:tracking-tight md:text-6xl md:tracking-tight">
+        <span className="block xl:inline">Create an Image</span>
+        </h1>
+
+        <p>
+          <span className="block text-gray-500 text-xl lg:max-w-3xl">
+            Create an image with the help of Open AI
+          </span>
+        </p>
+
       <form>
-        <label htmlFor="prompt">Prompt</label>
+        <label htmlFor="prompt" className="mt-2 text-white">Prompt</label>
         <input
+          className="bg-gray-50 text-gray-900 text-sm rounded-lg outline-none focus:ring-purple-500 focus:outline-purple-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-purple-500 dark:focus:border-purple-500"
           type="text"
           id="prompt"
           value={prompt}
           onChange={(e) => setPrompt(e.target.value)}
         />
-        <label htmlFor="n">Number of Images</label>
+        {/* <label htmlFor="n" className="mt-2 text-white">Number of Images</label>
         <input
+          className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
           type="number"
           id="n"
           value={n}
           onChange={(e) => setN(e.target.value)}
-        />
+        /> */}
       </form>
-      {/* make a button */}
-
-      <button onClick={generateImages}>Generate Images</button>
+      <button onClick={generateImages} className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded mt-2">Click to Generate Image</button>
       {
-        images.length === 0 ? <p>Loading...</p> : 
-        images.map((image) => (
-        <img src={image.url} alt="A nice Photos" />
+        images?.length === 0 ? <p className="text-white">Waiting for a Prompt...</p> : 
+        images?.map((image) => (
+        <div>
+          <div className="py-2"/>
+        <img src={image.url} alt="A nice Photos"/>
+        </div>
       ))
       }
+      <br/>
+      <hr/>
+
     </div>
   );
 };
-
 export default MakePhoto;
--- src/components/TextComplete.jsx
+++ src/components/TextComplete.jsx
@@ -42,61 +42,73 @@ export default function TextCompletions(){
   return (
     <div>
 
-      <h1>Text Completion</h1>
-      <p>Click the button to generate a text completion</p>
-      {/* make a form */}
+        <div className="py-6"/>
+        <h1 className="text-4xl lg:max-w-3xl tracking-tight font-bold text-white sm:text-5xl sm:tracking-tight md:text-6xl md:tracking-tight">
+          <span className="block xl:inline">Text Completions</span>
+        </h1>
+
+        <p>
+          <span className="block text-gray-500 text-xl lg:max-w-3xl">
+            Generate text with the help of Open AI
+          </span>
+        </p>
+
       <form>
-        <label htmlFor="prompt">Prompt</label>
+        <label htmlFor="prompt" className="mt-2 text-white">Prompt</label>
         <input
           type="text"
           id="prompt"
           value={prompt}
+          className="bg-gray-50 text-gray-900 text-sm rounded-lg outline-none focus:ring-purple-500 focus:outline-purple-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-purple-500 dark:focus:border-purple-500"
           onChange={(e) => setPrompt(e.target.value)}
         />
-
-        <label htmlFor="temperature">Temperature</label>
+        <label htmlFor="temperature" className="mt-2 text-white">Temperature</label>
         <input
           type="number"
           id="temperature"
           value={temperature}
+          className="bg-gray-50 text-gray-900 text-sm rounded-lg outline-none focus:ring-purple-500 focus:outline-purple-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-purple-500 dark:focus:border-purple-500"
           onChange={(e) => setTemperature(e.target.value)}
         />
-
-        <label htmlFor="max_tokens">Max Tokens</label>
+        <label htmlFor="max_tokens" className="mt-2 text-white">Max Tokens</label>
         <input
           type="number"
           id="max_tokens"
           value={max_tokens}
+          className="bg-gray-50 text-gray-900 text-sm rounded-lg outline-none focus:ring-purple-500 focus:outline-purple-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-purple-500 dark:focus:border-purple-500"
           onChange={(e) => setMax_tokens(e.target.value)}
         />
-
-        <label htmlFor="top_p">Top P</label>
+        <label htmlFor="top_p" className="mt-2 text-white">Top P</label>
         <input
           type="number"
           id="top_p"
           value={top_p}
+          className="bg-gray-50 text-gray-900 text-sm rounded-lg outline-none focus:ring-purple-500 focus:outline-purple-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-purple-500 dark:focus:border-purple-500"
           onChange={(e) => setTop_p(e.target.value)}
+
         />
 
-        <label htmlFor="frequency_penalty">Frequency Penalty</label>
+        <label htmlFor="frequency_penalty" className="mt-2 text-white">Frequency Penalty</label>
         <input
           type="number"
           id="frequency_penalty"
           value={frequency_penalty}
+          className="bg-gray-50 text-gray-900 text-sm rounded-lg outline-none focus:ring-purple-500 focus:outline-purple-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-purple-500 dark:focus:border-purple-500"
           onChange={(e) => setFrequency_penalty(e.target.value)}
         />
 
-        <label htmlFor="presence_penalty">Presence Penalty</label>
+        <label htmlFor="presence_penalty" className="mt-2 text-white">Presence Penalty</label>
         <input
           type="number"
           id="presence_penalty"
           value={presence_penalty}
+          className="bg-gray-50 text-gray-900 text-sm rounded-lg outline-none focus:ring-purple-500 focus:outline-purple-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-purple-500 dark:focus:border-purple-500"
           onChange={(e) => setPresence_penalty(e.target.value)}
         />
       </form>
 
-      <button onClick={Request}>Request</button>
-      <p>{response && response.choices[0].text}</p>
+      <button onClick={Request} className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded mt-2">Request</button>
+      <p className="text-white">{response && response.choices[0].text}</p>
     </div>
   );
 }
