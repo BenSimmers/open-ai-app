59b7cbace5b46b85692036e566c2ef3e06fd77e7
Benjamin Simmers <bej1380@gmail.com>
2023-02-20 00:37:37 +1000

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,75 +6,37 @@ function Navigation() {
   return (
     <>
       <BrowserRouter>
-        <nav class="px-2 sm:px-4 py-2.5 bg-indigo-700 fixed w-full z-20 top-0 left-0 border-b border-indigo-700">
-          <div class="container flex flex-wrap items-center justify-between mx-auto">
-            <a href="/" class="flex items-center">
-              <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white drop-shadow">
-                Another AI tool
+        <nav class="flex items-center justify-between flex-wrap bg-indigo-700 p-6">
+          <a href="/" class="flex items-center flex-shrink-0 text-white mr-6">
+            <div class="flex items-center flex-shrink-0 text-white mr-6">
+              <span class="font-semibold text-xl tracking-tight">
+                Another Ai Image Generator
               </span>
-            </a>
-            <div class="flex md:order-2">
-              <a href="https://github.com/BenSimmers/open-ai-app">
-              <button
-                type="button"
-                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 shadow"
+            </div>
+          </a>
+
+          <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+            <div class="text-sm lg:flex-grow">
+              <a
+                href="/makeaphoto"
+                class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4 border border-black rounded p-2 shadow-lg w-max"
+              >
+                Text to Image
+              </a>
+            </div>
+            <div>
+              <a
+                href="https://github.com/BenSimmers/open-ai-app"
+                class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
               >
                 Github
-              </button>
               </a>
-              <button
-                data-collapse-toggle="navbar-sticky"
-                type="button"
-                class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                aria-controls="navbar-sticky"
-                aria-expanded="false"
-              >
-                <span class="sr-only">Open main menu</span>
-                <svg
-                  class="w-6 h-6"
-                  aria-hidden="true"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                    clip-rule="evenodd"
-                  ></path>
-                </svg>
-              </button>
-            </div>
-            <div
-              class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
-              id="navbar-sticky"
-            >
-              <ul class="flex flex-col p-4 mt-4 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0">
-                <li>
-                  <a
-                    href="/makeaphoto"
-                    class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:p-0 dark:text-white"
-                    aria-current="page"
-                  >
-                    Make a photo
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                  >
-                    Text Generation - coming soon
-                  </a>
-                </li>
-              </ul>
             </div>
           </div>
         </nav>
-
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/makeaphoto" element={<MakePhoto />} />
+          <Route path="/photo" element={<MakePhoto />} />
         </Routes>
       </BrowserRouter>
     </>
@@ -83,24 +45,29 @@ function Navigation() {
 
 function Home() {
   return (
-    <div class="h-screen w-screen">
-      <div class="bg-indigo-700 w-full h-full p-28 flex items-center justify-center">
-        <div class="max-w-3xl flex h-full m-4 md:m-6">
-          <div class="relative group sm:w-full md:w-1/2 hover:bg-indigo-800 border-t border-l border-b border-r md:border-r-0 bg-opacity-5">
-            <h1 class="pl-2 pr-2 md:pl-8 mt-6 md:mt-8 text-2xl text-white font-serif transform duration-300 uppercase leading-snug">
-              Another AI Image Tool
-            </h1>
-            <p class="pl-2 pr-2 md:pl-8 mt-2 md:mt-4 text-white text-sm md:text-base leading-relaxed">
-              An easy to use image generator using Open AI's GPT-3 API built
-              with React and TailwindCSS
-            </p>
-            <p class="absolute bottom-1 p-4 md:pl-8 text-white text-xs">
-              More to come!
-            </p>
-          </div>
-          <div class="md:w-1/2 hidden md:block bg-indigo-700">
-            <img src="logo.png" alt="logo" class="h-full" />
-          </div>
+    <div className="justify-center items-center flex flex-col h-screen">
+      <div
+        class="flex flex-col items-center  border border-gray-200 shadow md:flex-row md:max-w-xl"
+      >
+        <img
+          class="object-cover w-full h-96 md:h-auto md:w-48"
+          src="logo.png"
+          alt=""
+        />
+        <div class="flex flex-col justify-between p-4 leading-normal">
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-white">
+            Another Ai Image Generator
+          </h5>
+          <p class="mb-3 font-normal text-gray-300">
+             Minimal application that uses the Open Ai API to generate images with more to come.
+          </p>
+
+          <Link to="/photo">
+            <button class="bg-gray-800 text-white rounded p-2">
+              Make a Photo
+            </button>
+          </Link>
+
         </div>
       </div>
     </div>
@@ -110,7 +77,17 @@ function Home() {
 export default function App() {
   return (
     <div className="bg-indigo-700">
-      <Navigation />
+      {/* <Navigation /> */}
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/photo" element={<MakePhoto />} />
+          <Route path="/*" element={<h1>404</h1>} />
+        </Routes>
+      </BrowserRouter>
+
+
     </div>
   );
 }
--- src/components/MakePhoto.jsx
+++ src/components/MakePhoto.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { Configuration, OpenAIApi } from "openai";
-
+import { Link } from "react-router-dom";
 /**
  * config is the configuration object for the API client.
  */
@@ -24,7 +24,6 @@ export default function MakePhoto() {
       size: "1024x1024",
     });
 
-
     return response.data.data[0].url;
   };
   useEffect(() => {
@@ -34,7 +33,6 @@ export default function MakePhoto() {
     };
     getImages();
   }, [prompt]);
-
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -46,26 +44,70 @@ export default function MakePhoto() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-indigo-700">
-      <h1 className="text-black">Make a Photo</h1>
-      <input
-        type="text"
-        className="bg-gray-800 text-white rounded p-2"
-        placeholder="Enter a prompt"
-        value={prompt}
-        onChange={(e) => setPrompt(e.target.value)}
-      />
-      <div className="py-2" />
-      <button
-        className="bg-gray-800 text-white rounded p-2"
-        onClick={handleSubmit}
-      >
-        Generate Image
-      </button>
+    // <div className="">
+    //   <h1 className="text-black">Make a Photo</h1>
+    //   <input
+    //     type="text"
+    //     className="bg-gray-800 text-white rounded p-2"
+    //     placeholder="Enter a prompt"
+    //     value={prompt}
+    //     onChange={(e) => setPrompt(e.target.value)}
+    //   />
+    //   <div className="py-2" />
+    //   <button
+    //     className="bg-gray-800 text-white rounded p-2"
+    //     onClick={handleSubmit}
+    //   >
+    //     Generate Image
+    //   </button>
 
-      {/* map the images */}
-      <img src={images} alt="Generated Image" />
+    //   {/* map the images */}
+    //   <img src={images} alt="Generated Image" />
+
+    // </div>
+
+    // make a nice form that looks like the home page
+    <div>
+      <div className="justify-center items-center flex flex-col h-screen">
+      <div class="flex flex-col items-center  border border-gray-200 shadow md:flex-row md:max-w-xl">
+        <div class="flex flex-col justify-between p-4 leading-normal">
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-white">
+            Text to Image
+          </h5>
+          <p class="mb-3 font-normal text-gray-300">
+            Enter a prompt and generate an image
+          </p>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              className="bg-gray-800 text-white rounded p-2"
+              placeholder="Enter a prompt"
+              value={prompt}
+              onChange={(e) => setPrompt(e.target.value)}
+            />
+            <div className="py-2" />
+            <button
+              className="bg-gray-800 text-white rounded p-2"
+              onClick={handleSubmit}
+            >
+              Generate Image
+            </button>
+          </form>
+
+
+        </div>
+        {/* <img class="object-cover w-full h-96 md:h-auto md:w-48" src="logo.png" alt=""/> */}
+
+        
+        <img class="object-cover w-full h-96 md:h-auto md:w-48" src={images ? images : "logo.png"} alt=""/>
+
+
+      </div>
+    </div>
+
+
+
 
     </div>
   );
-};
+}
